6171b93b2092b64e10c2f0e07dbf65036351ba6e
MANOJ-SUNDARALINGAM <110149229+MANOJ-SUNDARALINGAM@users.noreply.github.com>
2026-01-08 14:46:55 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+(function () {
+  console.log("INSTANDA external JS loaded");
+
+  function waitForElement(selector, callback) {
+    var observer = new MutationObserver(function () {
+      var el = document.querySelector(selector);
+      if (el) {
+        callback(el);
+        observer.disconnect();
+      }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+
+  // Example: hide Name Insured question
+  waitForElement('[data-question-id="NameInsured"]', function (el) {
+    el.style.display = "none";
+  });
+
+})();
