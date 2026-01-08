f980942ccbcc6d1f9813d3a9a9617f0983a76a0d
MANOJ-SUNDARALINGAM <110149229+MANOJ-SUNDARALINGAM@users.noreply.github.com>
2026-01-08 15:15:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
-alert("External INSTANDA JS loaded");
-
 (function () {
-  console.log("INSTANDA external JS loaded");
 
-  function waitForElement(selector, callback) {
-    var observer = new MutationObserver(function () {
-      var el = document.querySelector(selector);
-      if (el) {
-        callback(el);
-        observer.disconnect();
-      }
-    });
-    observer.observe(document.body, { childList: true, subtree: true });
+  function makeRoofAgeReadonly() {
+    var roofAgeField = document.getElementById("QQ_Property_AgeofRoof_NUM");
+    if (roofAgeField) {
+      roofAgeField.readOnly = true;
+      roofAgeField.classList.add("readonly");
+      console.log("Roof Age field set to readonly");
+      return true;
+    }
+    return false;
   }
 
-  // Example: hide Name Insured question
-  waitForElement('[data-question-id="NameInsured"]', function (el) {
-    el.style.display = "none";
-  });
+  // Try immediately
+  if (makeRoofAgeReadonly()) return;
+
+  // Retry until element appears
+  var interval = setInterval(function () {
+    if (makeRoofAgeReadonly()) {
+      clearInterval(interval);
+    }
+  }, 300);
 
 })();
-//
-var roofAgeField = document.getElementById("QQ_Property_AgeofRoof_NUM");
-if (roofAgeField) {
-    roofAgeField.readOnly = true;
-    roofAgeField.classList.add("readonly");
-}
