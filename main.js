3566598ea05b17ac1a581629df3f4a76467cdfaf
MANOJ-SUNDARALINGAM <110149229+MANOJ-SUNDARALINGAM@users.noreply.github.com>
2026-01-08 15:22:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("JS version 5");
+(function () {
+
+  function applyReadonly() {
+    var roofAgeField = document.getElementById("QQ_Property_AgeofRoof_NUM");
+    if (!roofAgeField) return false;
+
+    roofAgeField.readOnly = true;
+    roofAgeField.classList.add("readonly");
+
+    console.log("Roof Age set to readonly");
+    return true;
+  }
+
+  // Try immediately
+  if (applyReadonly()) return;
+
+  // Retry until INSTANDA creates the field
+  var interval = setInterval(function () {
+    if (applyReadonly()) {
+      clearInterval(interval);
+    }
+  }, 300);
+
+})();
