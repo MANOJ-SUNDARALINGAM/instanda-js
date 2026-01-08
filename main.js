cedde204b9cdc6440eea5fbbac8b301c48dbf1c2
MANOJ-SUNDARALINGAM <110149229+MANOJ-SUNDARALINGAM@users.noreply.github.com>
2026-01-08 16:28:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-<script>
-(function() {
-    var script = document.createElement("script");
-    script.src = "https://manoj-sundaralingam.github.io/instanda-js/main.js";
-    script.type = "text/javascript";
-    script.defer = true;
-    script.onload = function() {
-        console.log("✅ INSTANDA successfully loaded main.js from GitHub");
-    };
-    script.onerror = function() {
-        console.error("❌ Failed to load main.js from GitHub");
-    };
-    document.head.appendChild(script);
-})();
-</script>
+https://cdn.jsdelivr.net/gh/manoj-sundaralingam/instanda-js@main/main.js?v=2
